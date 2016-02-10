6255378de5a738b35bc73f632ce3372d1d9db76b
j-n4m4573 <jmg220p@gmail.com>
2016-02-10 14:53:02 -0800

    first commit

---
+++ BeefstagramSwiftLogin/Data.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  BeefstagramSwiftLogin
+//
+//  Created by Jamar Gibbs on 2/10/16.
+//  Copyright © 2016 M1ndful M3d1a. All rights reserved.
+//
+
+import Foundation
---
+++ BeefstagramSwiftLogin/Media.swift
@@ -0,0 +1,9 @@
+//
+//  Media.swift
+//  BeefstagramSwiftLogin
+//
+//  Created by Jamar Gibbs on 2/10/16.
+//  Copyright © 2016 M1ndful M3d1a. All rights reserved.
+//
+
+import Foundation
